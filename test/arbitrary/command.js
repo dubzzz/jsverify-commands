f139cafb37f0093ab5379843c33648a792d8d42a
Nicolas DUBIEN <github@dubien.org>
2017-10-02 00:22:46 +0200

    [test] switch to property 'using asked parameter types'

--- test/arbitrary/command.js
+++ test/arbitrary/command.js
@@ -17,16 +17,45 @@ describe('command', function() {
             assert.ok(Array.isArray(v.parameters), 'parameters is an array');
             assert.equal(v.parameters.length, 0, 'parameters array is empty');
         });
+
         it('should instantiate using asked parameter types', function() {
-            const arb = command(MyEmptyClass, jsc.nat, jsc.array(jsc.nat), jsc.constant(5));
-            var v = arb.generator(GENSIZE);
-            assert.ok(v.command instanceof MyEmptyClass, 'command instance of MyEmptyClass');
-            assert.ok(Array.isArray(v.parameters), 'parameters is an array');
-            assert.equal(v.parameters.length, 3, 'parameters array contains 3 elements');
-            assert.equal(typeof(v.parameters[0]), "number", 'first parameter is a number');
-            assert.ok(Array.isArray(v.parameters[1]), 'second parameter is an array');
-            assert.equal(typeof(v.parameters[2]), "number", 'third parameter is a number');
-            assert.deepEqual(v.command.params, v.parameters, "parameters are the one used during instantiation");
+            const knownArbs = {
+                "bool" : {
+                    arb: jsc.bool,
+                    check: v => v === true || v === false
+                },
+                "nat"  : {
+                    arb: jsc.nat,
+                    check: v => typeof(v) === 'number'
+                },
+                "oneof": {
+                    arb: jsc.oneof(jsc.constant(5), jsc.constant(42)),
+                    check: v => v === 5 || v === 42
+                },
+                "array": {
+                    arb: jsc.array(jsc.nat),
+                    check: v => Array.isArray(v) && v.find(i => typeof(i) !== 'number') === undefined
+                },
+            };
+            const allowedArbs = jsc.oneof.apply(this, Object.keys(knownArbs).map(v => jsc.constant(v)));
+
+            jsc.assert(jsc.forall(jsc.array(allowedArbs), function(arbsName) {
+                const arb = command.apply(this, [MyEmptyClass].concat(arbsName.map(v => knownArbs[v].arb)));
+                var v = arb.generator(GENSIZE);
+                var success = true;
+
+                success = success && v.command instanceof MyEmptyClass;
+                success = success && Array.isArray(v.parameters);
+                success = success && v.parameters.length === arbsName.length;
+                success = success && v.command.params.length === arbsName.length;
+
+                for (var idx = 0 ; idx !== arbsName.length ; ++idx) {
+                    success = success && knownArbs[arbsName[idx]].check(v.parameters[idx]);
+                    success = success && v.parameters[idx] == v.command.params[idx];
+                }
+
+                return success;
+            }));
         });
     });
 });
