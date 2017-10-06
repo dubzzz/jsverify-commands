0ecb971b85f14b4269fa6d5dc0970add714af660
Nicolas DUBIEN <github@dubien.org>
2017-10-07 01:11:10 +0200

    split forall into two entities

--- src/runner/forall.js
+++ src/runner/forall.js
@@ -1,27 +1,9 @@
 "use strict";
 const jsc = require('jsverify');
-
-var runall = async function(actions, state, model) {
-    for (var idx = 0 ; idx != actions.length ; ++idx) {
-        var ac = actions[idx];
-        if (ac.check(model)) {
-            ac.hasStarted = true;
-            if (! await ac.run(state, model)) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
+const {runner} = require('./runner');
 
 var forallImpl = function(arbSeed, arbCommands, warmup, teardown) {
-    return jsc.forall(arbSeed, arbCommands, async function(seed, commands) {
-        var actions = commands.map(c => c.command);
-        var {state, model} = await warmup(seed);
-        var result = await runall(actions, state, model);
-        await teardown();
-        return result;
-    });
+    return jsc.forall(arbSeed, arbCommands, runner(warmup, teardown));
 };
 
 var DEFAULT_SEED_GEN = jsc.constant(undefined);
---
+++ src/runner/runner.js
@@ -0,0 +1,26 @@
+var runall = async function(actions, state, model) {
+    for (var idx = 0 ; idx != actions.length ; ++idx) {
+        var ac = actions[idx];
+        if (ac.check(model)) {
+            ac.hasStarted = true;
+            if (! await ac.run(state, model)) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+var runner = function(warmup, teardown) {
+    return async function(seed, commands) {
+        var actions = commands.map(c => c.command);
+        var {state, model} = await warmup(seed);
+        var result = await runall(actions, state, model);
+        await teardown();
+        return result;
+    };
+};
+
+module.exports = {
+    runner: runner
+};
