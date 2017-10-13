edcb4d38bb53ebc523d15758b24c8a6e0725bb01
Nicolas DUBIEN <github@dubien.org>
2017-10-13 21:00:20 +0200

    Halve array during shrinker
    Switch to custom implementation of array shrinker. The one currently provided by jsverify remove one element at a time. We can imagine halving it.
    
    Fixes #4

--- src/arbitrary/commands.js
+++ src/arbitrary/commands.js
@@ -1,11 +1,14 @@
 "use strict";
 const jsc = require('jsverify');
+const lazyseq = require("lazy-seq");
 
 var cloneObject = function(o) {
     var cloned = {};
     Object.keys(o).filter(v => v != "hasStarted").forEach(k => cloned[k] = o[k]);
     return cloned;
 };
+
+
 
 var jscCommandsArray = function(gen, maxSize) {
     /**
@@ -16,6 +19,24 @@ var jscCommandsArray = function(gen, maxSize) {
      *   return Math.max(Math.round(Math.log(size + 1) / Math.log(2), 0));
      * }
      */
+    var shrinkImpl = function(arr) {
+        if (arr.length === 0) {
+            return lazyseq.nil;
+        }
+
+        var x = arr[0];
+        var xs = arr.slice(1);
+
+        var cuts = [];
+        for (let start = Math.floor(xs.length/2) ; start !== 0 ; start = Math.floor(start/2)) {
+            cuts.push(start);
+        }
+        cuts.push(0);
+
+        return lazyseq.fromArray(cuts.map(v => xs.slice(v)))
+            .append(gen.shrink(x).map(xp => [xp].concat(xs)))
+            .append(shrinkImpl(xs).map(xsp => [x].concat(xsp)));
+    };
     return jsc.bless({
         generator: (size) => {
             var arrsize = jsc.random(0, maxSize);
@@ -31,7 +52,7 @@ var jscCommandsArray = function(gen, maxSize) {
                     arr.splice(idx--, 1);
                 }
             }
-            return jsc.array(gen).shrink(arr)
+            return shrinkImpl(arr)
                 .map(narr => narr.map(cloneObject));
         },
         show: jsc.array(gen).show
