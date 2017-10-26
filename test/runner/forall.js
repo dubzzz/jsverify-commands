d0bcec8fc54e24b166397d6cfe12a1f21af8535e
Nicolas DUBIEN <github@dubien.org>
2017-10-26 02:25:27 +0200

    add tests for forall

---
+++ test/runner/forall.js
@@ -0,0 +1,46 @@
+"use strict";
+const assert = require('assert');
+const jsc = require('jsverify');
+
+const {forall} = require('../../src/runner/forall');
+
+const DEFAULT_WARMUP = async (seed) => new Object({state: {}, model: {}});
+const DEFAULT_TEARDOWN = async (state, model) => {};
+
+describe('forall', function() {
+    it('should be able to be called without any seed', async function() {
+        let settings = {};
+        let hasCalledWarmup = false;
+        let hasCalledTeardown = false;
+        const wup = (seed) => {
+            hasCalledWarmup = seed === undefined;
+            return {state: {}, model: {}};
+        };
+        const tdown = (state, model) => {
+            hasCalledTeardown = state !== undefined && model !== undefined;
+        };
+        const out = await jsc.assert(forall(jsc.constant([]), wup, tdown, settings));
+        assert.strictEqual(settings.metrics_output, undefined, "no record for metrics");
+        return out && hasCalledWarmup && hasCalledTeardown;
+    });
+    it('should be able to be called with a seed generator', async function() {
+        let settings = {};
+        let hasCalledWarmup = false;
+        let hasCalledTeardown = false;
+        const wup = (seed) => {
+            hasCalledWarmup = Array.isArray(seed) && seed.every(i => typeof(i) === 'number');
+            return {state: {}, model: {}};
+        };
+        const tdown = (state, model) => {
+            hasCalledTeardown = state !== undefined && model !== undefined;
+        };
+        const out = await jsc.assert(forall(jsc.array(jsc.integer), jsc.constant([]), wup, tdown, settings));
+        assert.strictEqual(settings.metrics_output, undefined, "no record for metrics");
+        return out && hasCalledWarmup && hasCalledTeardown;
+    });
+    it('should record metrics if asked to', async function() {
+        let settings = {metrics: true};
+        const out = await jsc.assert(forall(jsc.constant([]), DEFAULT_WARMUP, DEFAULT_TEARDOWN, settings));
+        return out && settings.metrics_output !== undefined;
+    });
+});
